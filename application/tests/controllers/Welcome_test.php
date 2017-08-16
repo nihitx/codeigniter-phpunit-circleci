a15c03fc56a422e7fa6ce27b6d874a31e31dd395
nihitx <masnadhossain@live.com>
2017-08-16 16:20:05 +0300

    welcome

--- application/tests/controllers/Welcome_test.php
+++ application/tests/controllers/Welcome_test.php
@@ -13,7 +13,7 @@ class Welcome_test extends TestCase
 	public function test_index()
 	{
 		$output = $this->request('GET', 'welcome/index');
-		$this->assertContains('<title>Welcome to CodeIgniter</title>', $output);
+		$this->assertContains('<title>Welcome to Masnad</title>', $output);
 	}
 
 	public function test_method_404()
